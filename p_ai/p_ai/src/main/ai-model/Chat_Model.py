a52d49a17157533e4e68d02d9045557da393a9e6
JAEMANJO <jjmyoucandoit@gmail.com>
2024-08-27 15:11:12 +0900

    PJ

--- p_ai/p_ai/src/main/ai-model/Chat_Model.py
+++ p_ai/p_ai/src/main/ai-model/Chat_Model.py
@@ -43,16 +43,33 @@ class LeaningAI:
          """
          모델을 학습하는 함수. 질문과 답변을 기반으로 KNN 모델을 학습.
          """
-         if self.questions or self.answers
+         if not self.questions or not self.answers:
+             print("Answers are not engluht for traning. Default response will be used.")
+             self.model = None   # 모델이 없는 상태로 유지
+             return
 
+         # 질문 데이터를 TF-IDF 벡터화
+         X = self.vectorizer.fit_transform(self.questions)
+
+         # KNN 모델을 학습
+         self.model = KNeighborsClassifier(n_neighbors=1)
+         self.model.fit(X, self.answers)
+
+   def get_answer(self, user_question):
+       """
+       사용자 질문에 대한 답변을 반환하는 함수.
+       """
+       if self.model is None:
+         return "Sorry, Don't have enough data to answer that right now."
+   
+         # 사용자 질문을 TF-IDF 벡터로 변환
+       user_question_vec = self.vectorizer.transform([user_question])
+
+         # 가장 유사한 질문을 찾고 해당 답변 반환
+       answer = self.model.predict(user_question_vec)[0]
+       return answer
          
 
-
-
-         
-         else:
-            print(f"Failed to fetch data. Status code: {response.status_code}")
-            return None, None
             
 
 
